36415ab1b06bf82e3525d1486e078d139555c7a1
WarinCode <warinsaipanya471@outlook.co.th>
2025-01-31 14:46:40 +0700

    Update user interface and fetch function with error handling

--- Lab06/user.js
+++ Lab06/user.js
@@ -1,25 +1,38 @@
 const url = "https://jsonplaceholder.typicode.com/users"
 const target = document.querySelector("#showData")
 let htmlText = ""
+const data = [];
 
 async function readJson() {
-    const res = await fetch(url)
-    const users = await res.json()
-
-    for (const user of users) {
-        htmlText += `
-        <div class="card m-4 border border-2 rounded-3 shadow p-2" style="width: 250px; height: max-content;">
-            <div class="card-body">
-                <h5 class="card-title fw-bold mb-4">${user.name}</h5>
-                <p class="card-text">${user.email}</p>
-                <p class="card-text">${user.company.name}</p>
-                <p class="card-text">${user.company.catchPhrase}</p>
-                <a href="#" class="btn btn-primary">Read More</a>
+    try {
+        const res = await fetch(url);
+        const users = await res.json();
+    
+        for (const user of users) {
+            htmlText += `
+            <div class="card m-4 border border-2 rounded-3 shadow p-2" style="width: 250px; height: max-content;">
+                <div class="card-body">
+                    <h5 class="card-title fw-bold mb-4">${user.name}</h5>
+                    <p class="card-text">${user.email}</p>
+                    <p class="card-text">${user.company.name}</p>
+                    <p class="card-text">${user.company.catchPhrase}</p>
+                    <button class="btn btn-primary">Read More</button>
+                </div>
             </div>
-        </div>
-        `
+            `;
+            data.push(user);
+        }
+        target.innerHTML = htmlText;
+    } catch(e){
+        console.error(e?.message);
     }
-    target.innerHTML = htmlText;
 }
 
-readJson();
+readJson().then(() => {
+    const buttons = document.querySelectorAll(".btn");
+    buttons.forEach((btn, i) => {
+        btn.addEventListener("click", (e) => {
+            alert(`Name: ${data[i].name}\nPhone: ${data[i].phone}`);
+        })
+    })
+});
